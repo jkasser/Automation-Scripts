c02f55a74d7a41cf9a558c6bde49d698a1526227
jkasser <jkasser@gmail.com>
2017-03-27 20:20:47 -0600

    fixes

--- PerfLogger.py
+++ PerfLogger.py
@@ -3,14 +3,19 @@ import sys
 import datetime
 import traceback
 import time
+import itertools
 from subprocess import call, PIPE, Popen
 from csv import reader
 
 
 def getInputs():
-	programToPoll = raw_input('Program/Process to check (no extension needed)?\n')
-	sampleInterval = raw_input('How frequently would you like to sample (in seconds)?\n')
-	runDuration = raw_input('How long would you like PerfLogger to run? (in seconds and greater than the sample interval\n')
+	"""
+	We want to use raw inputs here because if we used sys.argv then the inputs can be entered in the wrong order, etc etc
+	this really limits the potential for screw ups and allows us to add in some helper text since we arent using a gui
+	"""
+	programToPoll = raw_input('Program/Process to check? (e.g. Chrome - no extension is needed)\n')
+	sampleInterval = raw_input('In seconds, how frequently would you like to sample? (e.g. 1)\n')
+	runDuration = raw_input('In Seconds, how long would you like PerfLogger to run? (e.g 60)\n')
 	argList = [programToPoll, sampleInterval, runDuration]
 	return argList
 
@@ -19,32 +24,46 @@ def log(msg):
 	print now, msg
 
 def setUp(argList):
+	"""
+	Again, I don't think we need this, a throwback to when I was passing in args on the command line
+	"""
 	if len(argList) < 3:
 		log('This script requires 3 arguments! Please try again')
 		return
 	else:
 		checkedArgList = checkArgs(argList)
+
 	return checkedArgList
 
 def checkArgs(argList):
 	prgrm = argList[0]
 	checkProgram(prgrm)
 	for arg in argList[1:]:
+		"""
+		We don't need this bit anymore, its coming in via CMD since we are packaging this as an exe
+		therefore the variables will come in as a string and we have help text
 		if isinstance(arg, (int, long)):
 			continue
 		else:
 			log('Argument not valid! Value must be an integer')
-			try:
-				log('Trying to convert string to int...')
-				newArg = int(arg)
-				for i in (i for i,x in enumerate(argList) if x == arg):
-					argList[i] = newArg
-			except Exception:
-				log(str(traceback.format_exc()))
-				break
+		"""
+		try:
+			log('Trying to convert string to int...')
+			newArg = int(arg)
+			for i in (i for i,x in enumerate(argList) if x == arg):
+				argList[i] = newArg
+		except Exception:
+			log(str(traceback.format_exc()))
+
 	return argList
 
 def checkProgram(prgrm):
+	"""
+	The ony important check we actually want to do still. We will call tasklist and then pipe the 
+	output into a list. Then we can check the tasklist to see if the program entered is there. If the 
+	process isn't running then let's bail out because there is no point in continuing. If the process is running
+	then we can continue and use this program to pass it into perfmon.
+	"""
 	prgrmList = []
 	p = Popen('tasklist', stdin=PIPE, stdout=PIPE, stderr=PIPE)
 	output, err = p.communicate()
@@ -52,7 +71,7 @@ def checkProgram(prgrm):
 		log(str(prgrm)+' found as a running process!')
 	else:
 		log(str(prgrm)+' not found running, exiting program')
-		exit()
+		sys.exit(0)
 
 def createPerfmon(argList):
 	path =  os.path.expandvars('%UserProfile%\\Desktop')
@@ -72,7 +91,7 @@ def startPerfmon():
 	call('logman.exe start "PerfLogger"', shell=False)
 
 def stopPerfmon():
-	log('Attempting to stop any existing perfmon logger thaty may already be running')
+	log('Attempting to stop any existing perfmon logger that may already be running')
 	call('logman.exe stop "PerfLogger"', shell=False)
 
 def convertIt(fileList):
@@ -91,9 +110,8 @@ def averages(fileList):
 	with open(file, 'r') as f:
 		data = list(reader(f))
 
-	# memory list comprehension to convert strings to ints and average	
+	# memory list comprehension to convert strings to ints and average
 	memory = [int(i[1]) for i in data[1:]]
-	detectLeak(memory)
 	avgMemory = (sum(memory)/len(memory))
 
 	"""
@@ -101,7 +119,7 @@ def averages(fileList):
 	sometimes perfmon records null values and we have to handle this
 	we iterate through the list finding empty strings and replace them with 0
 	since the other values are turned as a string we do the same list comprehension
-	as the other metrics and just change the type ti int
+	as the other metrics and just change the type to int
 	"""
 	cpu = [i[2] for i in data[1:]]
 	for i,x in enumerate(cpu):
@@ -118,22 +136,49 @@ def averages(fileList):
 	log('Average Memory: '+str(avgMemory))
 	log('Average CPU: '+str(avgCpu))
 	log('Average Handles: '+str(avgHandle))
+	# call this down here so it's the last thing we do
+	detectLeak(memory)
 
 def detectLeak(memory):
+	"""
+	After many ideas this is what I finally settled on
+	Basically if we have more than 5 datapoints of memory consumption then
+	we will average out every 5 of them and add those points to a list.
+	We can check each average with the consecutive item in the list and see if the average
+	is increasing or decreasing, if the average is increasing then there is an indication
+	of a memory leak. In order to rule out outliers we want to make sure the trend
+	is above a certain amount. So we will subtract 1 from the leak potential if the average
+	is less than or equal to the next one in the list, singaling normal fluctuations.
+	"""
 	leakPotential = 0
-	baseline = memory[0]
-	for x in memory:
-		if x > baseline:
-			leakPotential += 1
-		elif x < baseline:
-			leakPotential -= 1
-	leakThreshold = .7*len(memory)
-	if leakPotential > leakThreshold:
-		log(str(leakPotential)+' leak potential over threshold of '+str(leakThreshold)+', potential memory leak detected!')
+	if len(memory) > 5:
+		sampleSet = []
+		y = 0
+		for x in range(1, len(memory)):
+			if (x % 5 == 0):
+				# print y, x
+				sampleSet.append(sum(memory[y:x])/5)
+				y = x
+		for dp in sampleSet:
+			if sampleSet.index(dp) < (len(sampleSet)-1):
+				if dp <= sampleSet[sampleSet.index(dp)+1]:
+					leakPotential -= 1
+				else:
+					leakPotential += 1
+			else:
+				break
+		if 3 >= leakPotential >= 1:
+			log('Slight possibility of a memory leak found! '+str(leakPotential)+' sampled averages increased more than decreased')
+		elif leakPotential > 3:
+			log('Good possibility of a memory leak found! '+str(leakPotential)+' sampled averages increased more than decreased')
+		elif leakPotential < 1:
+			log('No real possibility of a memory leak found! '+str(leakPotential)+' sampled averages increased more than decreased')
 	else:
-		log(str(leakPotential)+' leak potential, threshold of '+str(leakThreshold)+' not met')
+		log('Insufficient data points present! We need more than 5 datapoints to guess if there is a memory leak')
+
 
 def cleanUp(fileList):
+	# Clean up all the files on the desktop
 	try:
 		for file in fileList:
 			log('Attempting to cleanup file: '+str(file))
@@ -151,6 +196,7 @@ if __name__ == '__main__':
 	time.sleep(checkedArgList[2])
 	convertIt(fileList)
 	averages(fileList)
-	# cleanUp(fileList)
+	raw_input('\nPress any key to exit!\n')
+	cleanUp(fileList)
 
 
